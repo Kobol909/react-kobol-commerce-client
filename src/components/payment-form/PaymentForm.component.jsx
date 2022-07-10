0ba78feb645583495de49e5bb063863d029a0504
kobol909 <campana.fabien@me.com>
2022-07-10 13:13:11 +0200

    Troubleshooting Stripe API key | wrong key import in create-payment-intent.js. String is not being read correctly

--- src/components/payment-form/PaymentForm.component.jsx
+++ src/components/payment-form/PaymentForm.component.jsx
@@ -21,7 +21,7 @@ import { FormContainer, PaymentButton, PaymentFormContainer } from './PaymentFor
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-  const amount = useSelector(selectCartTotal);
+  const amount = useSelector(selectCartTotal) * 100;
   const currentUser = useSelector(selectCurrentUser);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
@@ -38,7 +38,7 @@ const PaymentForm = () => {
         authorization: `Bearer ${jwt.bearer}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ amount: amount * 100 })
+      body: JSON.stringify({ amount: amount })
     }).then((res) => {
       return res.json();
     });
@@ -48,10 +48,6 @@ const PaymentForm = () => {
       response,
       'amount :',
       amount,
-      'currentUser :',
-      currentUser,
-      'stripe :',
-      stripe,
       'response.paymentIntent :',
       response.paymentIntent
     );
